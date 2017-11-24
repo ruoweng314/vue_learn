d10a7ef8b885394b31d647afd6c943f7e3e7146a
xuyangyang <xuyangyang@clickplus.cn>
2017-11-24 09:47:31 +0800

    xuyangyang 2017、11、24
    learn  vue-cnode.js

--- learn_react/clickplus/src/PageClickplus.js
+++ learn_react/clickplus/src/PageClickplus.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import $ from 'jquery';
 import MenuClass from './style/CommonMenu-m.css';
 import Tabs from 'react-bootstrap/lib/Tabs';
 import Tab from 'react-bootstrap/lib/Tab';
@@ -7,7 +7,7 @@ import LayoutClass from './style/HomeLayout.less';
 import LineChart from './LineChart';
 import PieChart from './pieChart';
 import PredefinedRanges from './PredefinedRanges';
-
+import Button from 'react-bootstrap/lib/Button';
 export default class PageClickplus extends React.Component {
  constructor(props){
     super(props);
@@ -41,11 +41,39 @@ export default class PageClickplus extends React.Component {
                     ]
       }
     }
+
+    this.getCamptest = this.getCamptest.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
+
+  getCamptest(){
+    // $.ajax({
+    //   type:'get',
+    //   url:'ajax/getOverviewAmoutReportNew',
+    //   data:{"is_delete":1,"start_time":"2017-11-14","end_time":"2017-11-14"},
+    //   dataType:'json',
+    //   success:function(data){
+    //     console.log(data);
+    //   }
+    // })
+
+    fetch('ajax/getOverviewAmoutReportNew?is_delete=1&start_time=2017-11-14&end_time=2017-11-14',{method:'GET'})
+      .then((response) => response.json()) //下一步操作
+      .then((responseData) => {
+      console.log(responseData); //打印出来
+      // alert(responseData);
+      });
+   }
+
+  handleSelect(key) {
+    console.log(`selected ${key}`);
+   // this.setState({ key });
+  }
+
   render(){
     return (
       <div className={MenuClass.ClickplusContent}>
-        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+        <Tabs defaultActiveKey={1} onSelect={this.handleSelect} id="uncontrolled-tab-example">
           <Tab eventKey={1} title="数据报表">
             <div className={LayoutClass.layout} style={{width:'59%',float:'left'}}>
               <div className={LayoutClass.title}>
@@ -65,7 +93,7 @@ export default class PageClickplus extends React.Component {
             </div>
           </Tab>
           <Tab eventKey={2} title="组件点击图">
-           11111111
+            <Button bsStyle="primary" bsSize="small" onClick={this.getCamptest}>ajax请求事件</Button>
           </Tab>
           <Tab eventKey={3} title="热图分析">
             <PredefinedRanges />
